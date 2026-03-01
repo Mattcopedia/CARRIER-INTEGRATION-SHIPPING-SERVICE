67441716e43ce4a9e4b51325e9a57bef28373ffb
Mattcopedia <mathiasofeimun@gmail.com>
2026-03-01 22:36:12 +0100

    Initial carrier integration service implementation

--- dist/utils/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-exports.logger = {
-    info: (msg, meta) => console.log(`[INFO] ${msg}`, meta ?? ""),
-    warn: (msg, meta) => console.warn(`[WARN] ${msg}`, meta ?? ""),
-    error: (msg, meta) => console.error(`[ERROR] ${msg}`, meta ?? ""),
-};
-//# sourceMappingURL=logger.js.map
